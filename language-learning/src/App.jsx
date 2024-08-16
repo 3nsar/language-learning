eae4ce463264371b0d77914a0af5fb0d4e1bd24f
3nsar <84131267+3nsar@users.noreply.github.com>
2024-08-16 14:54:43 +0200

    commit

---
+++ language-learning/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='container'>
+      <div className='content'>
+          <button>PLAY</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
